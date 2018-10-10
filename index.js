474d1f6c10d2bca6ac9e2189e24e2ccce534bdb0
Felipe de Alencar Pinheiro <felipe.pinheiro.90@hotmail.com>
2018-10-10 10:34:10 -0300

    Change the structure to create a file with the publisher

--- index.js
+++ index.js
@@ -1,27 +1,5 @@
 require('dotenv').config()
 
-// Imports the Google Cloud client library
-const PubSub = require('@google-cloud/pubsub');
+const publish = require('./pub');
 
-// Creates a client
-const pubsub = new PubSub({
-  projectId: process.env.PROJECT_ID,
-  keyFilename: process.env.PATH_TO_KEY,
-});
-
-const topicName = 'dyp-topic';
-const data = JSON.stringify({ foo: 'bar' });
-
-// Publishes the message as a string, e.g. "Hello, world!" or JSON.stringify(someObject)
-const dataBuffer = Buffer.from(data);
-
-pubsub
-  .topic(topicName)
-  .publisher()
-  .publish(dataBuffer)
-  .then(messageId => {
-    console.log(`Message ${messageId} published.`);
-  })
-  .catch(err => {
-    console.error('ERROR:', err);
-  });
+publish();
---
+++ pub.js
@@ -0,0 +1,29 @@
+// Imports the Google Cloud client library
+const PubSub = require('@google-cloud/pubsub');
+
+// Creates a client
+const pubsub = new PubSub({
+  projectId: process.env.PROJECT_ID,
+  keyFilename: process.env.PATH_TO_KEY,
+});
+
+const topicName = 'dyp-topic';
+const data = JSON.stringify({ foo: 'bar' });
+
+const publish = () => {
+  // Publishes the message as a string, e.g. "Hello, world!" or JSON.stringify(someObject)
+  const dataBuffer = Buffer.from(data);
+
+  return pubsub
+    .topic(topicName)
+    .publisher()
+    .publish(dataBuffer)
+    .then(messageId => {
+      console.log(`Message ${messageId} published.`);
+    })
+    .catch(err => {
+      console.error('ERROR:', err);
+    });
+}
+
+module.exports = publish;
